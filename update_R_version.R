ba464f97b2afebab3eb99bc35217e224e882cf4d
ChanningC12 <chicheng2015@u.northwestern.edu>
2017-07-23 22:53:21 -0400

    Add files via upload

---
+++ update_R_version.R
@@ -0,0 +1,9 @@
+# http://bioinfo.umassmed.edu/bootstrappers/bootstrappers-courses/courses/rCourse/Additional_Resources/Updating_R.html
+# installing/loading the package:
+if(!require(installr)) {
+  install.packages("installr"); 
+  require(installr)
+} #load / install+load installr
+
+# using the package:
+updateR()
